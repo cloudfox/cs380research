ad427dce3a99133cd03aa6b0dfc8aec6b665670a
steven <steven.kugies@digipen.edu>
2022-03-19 15:11:24 -0700

    added basic squadagent coo set more folders to git ignore

---
+++ cs380researchproject/Source/cs380researchproject/Agent/SquadAgent.cpp
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SquadAgent.h"
+
+// Sets default values
+ASquadAgent::ASquadAgent()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root Comp"));
+	
+	MeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh"));
+	MeshComponent->SetupAttachment(RootComponent);
+
+	aiPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("Perception Component"));
+
+	sightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("Sight Config"));
+	aiPerceptionComponent->SetDominantSense(sightConfig->GetSenseImplementation());
+
+	sightConfig->SightRadius = 1000.0f;
+	sightConfig->LoseSightRadius = 1000.0f;
+	sightConfig->PeripheralVisionAngleDegrees = 60.0f;
+	sightConfig->DetectionByAffiliation.bDetectEnemies = true;
+	sightConfig->DetectionByAffiliation.bDetectNeutrals = true;
+	sightConfig->DetectionByAffiliation.bDetectFriendlies = true;
+	aiPerceptionComponent->ConfigureSense(*sightConfig);
+	
+}
+
+// Called when the game starts or when spawned
+void ASquadAgent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASquadAgent::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ASquadAgent::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ cs380researchproject/Source/cs380researchproject/Agent/SquadAgent.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+#include "SquadAgent.generated.h"
+
+
+UCLASS()
+class CS380RESEARCHPROJECT_API ASquadAgent : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ASquadAgent();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	class USkeletalMeshComponent* MeshComponent;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	class UAIPerceptionComponent* aiPerceptionComponent;
+
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+	UAISenseConfig_Sight* sightConfig;
+	
+private:
+
+	
+	
+	
+};
